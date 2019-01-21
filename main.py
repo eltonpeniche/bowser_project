8c22315a4a720b43c0ad84f42a11ca9095be3f5a
eltonpeniche <elton_nfs@hotmail.com>
2019-01-20 22:44:35 -0300

    bowser_project

---
+++ main.py
@@ -0,0 +1,66 @@
+# Importar modulo do sistema operacional
+import os
+
+rede = "192"
+
+os.system("sudo apt install arp-scan -y")
+os.system("echo '' > ips.txt")
+os.system("sudo arp-scan 192.168.100.0/24 >> ips.txt")
+
+#abre o arquivo que contém o endereço ip e mac dos computadores ativos na rede
+# e coloca na lista X
+ref_arquivo = open("ips.txt","r")
+linha = ref_arquivo.readline()
+x = []
+while linha:
+	if rede in linha:
+		valor1, valor2, *_ = linha.split()
+		x.append([valor1, valor2])
+	linha = ref_arquivo.readline()
+ref_arquivo.close()
+os.system("sudo rm -Rf ips.txt")
+
+#abre o arquivo que contém o endereço mac e o nome dos computadores ativos na rede
+# e os coloca numa lista Y
+ref_arquivo = open("mac_host.txt","r")
+linha = ref_arquivo.readline()
+y = []
+while linha:
+	valores = linha.split()
+	y.append(valores)
+	linha = ref_arquivo.readline()
+ref_arquivo.close()
+
+#como o comando "arp-scan" não traz o endereço na maquina local
+#este comando é usado para pegar o nome e ip do host local
+lista3 = [[os.popen("hostname -I").readline().rstrip('\n '), os.popen("hostname").readline().rstrip('\n ')]]
+
+#cria uma lista com o ip e nome do host a partir das litas X e Y
+aux = y.copy()  
+z = 0
+for i in y:
+	for j in x:
+		if(i[0]==j[1]):
+			lista3.append([j[0],i[1]])
+			aux.remove(i)
+	z+=1
+
+
+if len(aux) == 1:	
+	print("Sucesso")
+	for i in lista3:
+		print(i)
+else:
+	print("Computadores não podem ser acessados:")
+	for i in range(1,len(aux)):
+		print(aux[i][1])
+
+
+#grava no arquivo host a lista3
+arq = open('hosts', 'w')
+arq.writelines('127.0.0.1	localhost' + '\n\n')
+for i in lista3:
+	arq.writelines(i[0]+ ' ' +i[1] + '\n')
+arq.close()
+
+os.system("sudo mv hosts /etc/hosts")
---
+++ main2.sh
@@ -0,0 +1,150 @@
+
+#!/bin/bash
+master="LABCOMP2-PC01"
+rede="192.168.100.0/24"
+cam=$(pwd)
+
+
+sudo apt install net-tools
+sudo apt update
+sudo apt install build-essential manpages-dev -y
+#sudo apt install arp-scan -y
+
+
+clear
+configurarArquivoHosts(){
+	echo "---------------OPÇÃO(3/8)-----------------"
+	echo "CONFIGURANDO O ARQUIVO /etc/hosts         "
+	sudo python3 main.py
+	sleep 2
+}
+ms=3
+
+clear
+instalarNFS(){
+	echo "---------------Passo(4/8)-----------------"
+	echo "----------- Instalando o NFS--------------"
+	sleep 2
+	read -p "A maquina é slave(1) ou master(0)? " ms
+	echo ""
+
+	#+--------------Configuração do(s) slave(s)------------------------+
+	if [ $ms -eq 1 ]; then
+		
+		sudo apt install nfs-common -y
+		# criando o ponto de montagem
+		#sudo mount $master:/home/$USER /home/$USER
+		echo "$master:/home/$USER/Bowser /home/$USER/Bowser nfs" | sudo tee -a /etc/fstab
+		echo "$master:/home/$USER/.ssh /home/$USER/.ssh nfs" | sudo tee -a /etc/fstab
+		# Agora vamos verificar se as pastas foram montadas corretamente
+		#sudo mount -a
+		#echo ""
+		#df –h
+		
+
+	#== ============Configuração do Master==============================
+
+	elif [ $ms -eq 0 ]; then
+		
+		sudo apt-get install nfs-server -y
+		#Agora vamos editar o arquivo /etc/exports
+		mkdir /home/$USER/Bowser
+		mkdir /home/$USER/.ssh
+        echo "/home/$USER/Bowser *(rw,sync,no_subtree_check)" | sudo tee -a /etc/exports
+        echo "/home/$USER/.ssh *(rw,sync,no_subtree_check)" | sudo tee -a /etc/exports
+		sudo service nfs-kernel-server restart
+		sudo exports -a
+		sudo ufw allow from $rede
+		source ~/.bashrc
+		
+	fi
+	sleep 5
+}
+
+instalarSSH(){
+	echo "-------------Passo(5/8)------------------"
+	echo "-----------Instalando o SSH--------------"
+	sleep 2
+	if [ $ms -eq 3 ]; then
+		read -p "A maquina é slave(1) ou master(0)? " ms
+		echo ""
+	fi	
+
+	if [ $ms -eq 1 ]; then
+		sudo apt install openssh-server -y
+		
+	#== ============Configuração do Master==============================
+
+	elif [ $ms -eq 0 ]; then
+		#sudo apt-get install openssh-client openssh-server -y
+        sudo apt install openssh-client -y
+		#sudo apt install clusterssh -y
+		ssh-keygen
+		cd ~/.ssh
+		ssh-copy-id -i id_rsa.pub localhost
+		
+	fi
+
+	sleep 5
+}
+
+instacaoOpenMpi(){
+	echo ""
+	echo "-----------Passo(6/7)--------------------"
+	echo "------Instalando o OPEN MPI--------------"
+	sleep 2
+	cp $cam/openmpi-3.1.2.tar.gz ~/Documentos
+	cd ~/Documentos
+	tar -vzxf openmpi-3.1.2.tar.gz
+	cd openmpi-3.1.2
+	./configure --enable-orterun-prefix-by-default   
+	make;sudo make install
+
+	echo 'export PATH=/usr/local/bin:$PATH' | sudo tee -a ~/.bashrc
+	echo 'export LD_LIBRARY_PATH=/usr/local/lib:$LD_LIBRARY_PATH' | sudo tee -a ~/.bashrc
+	source ~/.bashrc
+	sudo rm -Rf ~/Documentos/openmpi-3.1.2 ~/Documentos/openmpi-3.1.2.tar.gz
+                
+
+	which mpicc
+	which mpirun
+	sleep 3
+
+}
+
+opcao=14
+while [ $opcao -ne 8 ]; do
+	
+	echo ""
+	echo " CONFIGURAR O NOME DA MÁQUINA---------------(1)"
+	echo " CONFIGURAR O ARQUIVO /etc/hosts------------(2)"
+	echo " INSTALAR E CONFIGURAR O NFS----------------(3)"
+	echo " INSTALAR E CONFIGURAR o SSH----------------(4)"	
+	echo " INSTALAR E CONFIGURAR  O OPEN MPI----------(5)"	
+	echo " TODAS AS OPÇÕES----------------------------(6)"	
+	echo " SAIR---------------------------------------(0)"
+	read -p "escolha uma opção de 0 a 8:  " opcao
+
+
+	if [ $opcao -eq 1 ]; then
+		mudarNome	
+	elif [ $opcao -eq 2 ]; then
+		configurarArquivoHosts
+	elif [ $opcao -eq 3 ]; then
+		instalarNFS
+	elif [ $opcao -eq 4 ]; then
+		instalarSSH	
+	elif [ $opcao -eq 5 ]; then
+		instacaoOpenMpi	
+	elif [ $opcao -eq 6 ]; then
+        configurarArquivoHosts
+		instacaoOpenMpi
+		instalarNFS	
+		instalarSSH
+	elif [ $opcao -eq 0 ]; then
+		break
+	else
+		echo "ERRO! TENTE NOVAMENTE..!!"
+	fi
+
+done
